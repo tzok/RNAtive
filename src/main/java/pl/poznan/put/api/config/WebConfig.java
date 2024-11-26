cac20dc27a9df44a51e54c7cb60528391d768a2b
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2024-11-26 08:53:18 +0100

    feat: Add CORS configuration with dynamic origin and method settings

--- src/main/java/pl/poznan/put/api/config/WebConfig.java
+++ src/main/java/pl/poznan/put/api/config/WebConfig.java
@@ -1,13 +1,30 @@
 package pl.poznan.put.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+  @Value("${cors.allowed-origin}")
+  private String allowedOrigin;
+
+  @Value("${cors.allowed-methods}")
+  private String allowedMethods;
+
   @Bean
   public RestTemplate restTemplate() {
     return new RestTemplate();
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins(allowedOrigin)
+        .allowedMethods(allowedMethods.split(","))
+        .allowedHeaders("*");
   }
 }
