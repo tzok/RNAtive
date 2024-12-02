33f23a219e8170e5246c284a8c135df2f2de8744
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2024-12-02 14:32:47 +0100

    feat: Add REST client for RNAlyzer service with session management

---
+++ src/main/java/pl/poznan/put/rnalyzer/MolProbityResponse.java
@@ -0,0 +1,20 @@
+package pl.poznan.put.rnalyzer;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record MolProbityResponse(Structure structure) {
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Structure(Description description, String clashscore, String pctRank,
+                          String rankCategory, String pctProbablyWrongSugarPuckers,
+                          String probablyWrongSugarPuckersCategory,
+                          String pctBadBackboneConformations,
+                          String badBackboneConformationsCategory,
+                          String pctBadBonds, String badBondsCategory,
+                          String pctBadAngles, String badAnglesCategory) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Description(String filename, String errors) {
+    }
+}
