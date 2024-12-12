cfb40e78ed973d54834972ddd71aa97d0a7c2e51
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2024-12-12 17:44:16 +0100

    fix: Improve error message for sequence length mismatch

--- src/main/java/pl/poznan/put/api/util/InvalidSequenceLengthException.java
+++ src/main/java/pl/poznan/put/api/util/InvalidSequenceLengthException.java
@@ -4,7 +4,7 @@ public class InvalidSequenceLengthException extends RuntimeException {
   public InvalidSequenceLengthException(int expectedLength, int actualLength) {
     super(
         String.format(
-            "Sequence length mismatch: expected %d residues, but got %d",
+            "Sequence length mismatch: reference sequence has %d residues, but the 3D model has %d",
             expectedLength, actualLength));
   }
 }
