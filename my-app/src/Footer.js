6c695709c4fff7b9d81e0404abc2711cc0bd9f75
StormageddonDarkLordOfAll <jmp@onet.com.pl>
2024-12-17 14:07:48 +0100

    logo in footer
    added logo in website footer

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import ImageSvg from "./RNAtive.svg";
-import logo from "./RNAtive2.svg";
+import logo from "./PP_logotyp_ANG_WHITE.png";
 const Footer = () => {
   return (
     <nav className="navbar">
@@ -15,10 +15,15 @@ const Footer = () => {
         </a> */}
       </div>
       <div className="navbar-center">
+        {/* <div style={{ width: "10%", textAlign: "center" }}>
+          <img src={logo} alt="PUT" style={{ height: "32px" }} />
+        </div> */}
+
         <p>
           This website is free to use. We do not require any cookies or login to
           access it.
         </p>
+        <img src={logo} alt="PUT" style={{ height: "64px" }} />
       </div>
       <div></div>
     </nav>
