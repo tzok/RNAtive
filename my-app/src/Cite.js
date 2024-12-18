682905fe4095456e10956437ae7e2a1b5361c80c
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2024-12-18 09:21:12 +0100

    feat: update content of Cite page

--- my-app/src/Cite.js
+++ my-app/src/Cite.js
@@ -1,51 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Navbar";
+import {Card} from "antd";
 
 const Cite = () => {
   return (
     <div>
       <header className="App-header">
-        <div class="rounded-border">
-          <p
-            style={{
-              fontSize:
-                "25px" /* This font size is set using a 'string value' */,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <b>CITE US</b>
-          </p>
-          <p
-            style={{
-              fontSize:
-                "18px" /* This font size is set using a 'string value' */,
-            }}
-          >
-            <b>
-              In any published work that has made use of RNAtive, please cite
-              the following paper:
-            </b>
-            <br></br>
-            <br></br>
-            "GREAT PAPER ABOUT LOREM"<br></br>
-            I. Ipsum, D. Dolor
-            <br></br>
-            <br></br>
-            <b>Other related works include:</b>
-            <br></br>
-            <br></br>
-            "Ighi kurumashu katarinuda magush ishi turbugha"<br></br>
-            A. Shagrat, G. Drublak
-            <br></br>
-            <br></br>
-            "Aghburz durbagu kirminudu bagur ishi darulu"<br></br>
-            I. Ishi, D. Ashi
-            <br></br>
-            <br></br>
-          </p>
+        <div style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "20px",
+        }}>
+          <Card title="Cite Us" style={{
+            width: "50%",
+          }}>
+            <p>Any published work which has made use of RNAtive should cite the following paper:</p>
+            <p>J. Pielesiak, M. Antczak, M. Szachniuk, T. Zok. RNAtive to recognize native-like structure in a set of RNA 3D models.</p>
+          </Card>
         </div>
       </header>
     </div>
