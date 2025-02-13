831a974025e75b8be75123117454a7ae867e2e8c
Tomasz Żok <tomasz.zok@cs.put.poznan.pl>
2025-02-13 11:48:06 +0100

    feat: Add CLI switch to configure MolProbity filter with validation

--- src/main/java/pl/poznan/put/api/CliRunner.java
+++ src/main/java/pl/poznan/put/api/CliRunner.java
@@ -2,9 +2,12 @@ package pl.poznan.put.api;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import pl.poznan.put.api.model.MolProbityFilter;
 
 @Component
 public class CliRunner implements CommandLineRunner {
+    private MolProbityFilter molProbityFilter = MolProbityFilter.GOOD_ONLY; // default value
+
     @Override
     public void run(String... args) throws Exception {
         if (args.length == 0) {
@@ -12,19 +15,46 @@ public class CliRunner implements CommandLineRunner {
             return;
         }
 
-        if (args[0].equals("--help")) {
-            printHelp();
-            return;
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "--help":
+                    printHelp();
+                    return;
+                case "--mol-probity":
+                    if (i + 1 < args.length) {
+                        try {
+                            molProbityFilter = MolProbityFilter.valueOf(args[i + 1].toUpperCase());
+                            i++; // skip the next argument since we consumed it
+                        } catch (IllegalArgumentException e) {
+                            System.err.println("Invalid MolProbity filter value. Valid values are: " +
+                                String.join(", ", getMolProbityValues()));
+                            return;
+                        }
+                    } else {
+                        System.err.println("--mol-probity requires a value");
+                        return;
+                    }
+                    break;
+                default:
+                    System.err.println("Unknown option: " + args[i]);
+                    printHelp();
+                    return;
+            }
         }
 
-        // TODO: Add your CLI logic here
-        System.out.println("Processing arguments: " + String.join(" ", args));
+        // Process with selected options
+        System.out.println("Processing with MolProbity filter: " + molProbityFilter);
     }
 
     private void printHelp() {
         System.out.println("Usage: java -jar application.jar [options]");
         System.out.println("Options:");
-        System.out.println("  --help     Show this help message");
-        // TODO: Add more options documentation
+        System.out.println("  --help                    Show this help message");
+        System.out.println("  --mol-probity <filter>    Set MolProbity filter level");
+        System.out.println("                            Valid values: " + String.join(", ", getMolProbityValues()));
+    }
+
+    private String[] getMolProbityValues() {
+        return new String[]{"GOOD_ONLY", "GOOD_AND_CAUTION", "ALL"};
     }
 }
