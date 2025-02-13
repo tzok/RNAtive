63089119698e882ee8ce69fffc996166b9db212a
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2025-02-13 15:27:24 +0100

    refactor: Simplify Spring Boot application startup with conditional web mode

--- src/main/java/pl/poznan/put/api/Application.java
+++ src/main/java/pl/poznan/put/api/Application.java
@@ -1,6 +1,5 @@
 package pl.poznan.put.api;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -11,13 +10,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class Application {
   public static void main(String[] args) {
     SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
-    
+
     String appMode = System.getenv("APP_MODE");
     if ("cli".equalsIgnoreCase(appMode)) {
       // Run in CLI mode
       builder.web(WebApplicationType.NONE);
     }
-    
+
     builder.run(args);
   }
 }
