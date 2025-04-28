ac621e1fb5707ddd8b5509cb7c2f099b6a97c4c3
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2025-04-28 13:44:17 +0200

    feat: Import JsonNode in ComputeController and ComputeService

--- src/main/java/pl/poznan/put/api/service/ComputeService.java
+++ src/main/java/pl/poznan/put/api/service/ComputeService.java
@@ -1,5 +1,6 @@
 package pl.poznan.put.api.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
